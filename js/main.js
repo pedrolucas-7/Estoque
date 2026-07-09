9edc33b735a72a01837a2195cc3a7e5641452cb2
Pedro <pedrolucasfarias2008@gmail.com>
2026-07-08 21:55:45 -0300

    atualzação botão de login

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// ==============================
+// StockFlow
+// Arquivo principal JavaScript
+// ==============================
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("StockFlow iniciado com sucesso!");
+});
